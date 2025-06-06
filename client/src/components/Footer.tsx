61804b5371ecbbff1e482e24d0abc63649167d56
Hamza Aziz <myselfhamzaaziz@gmail.com>
2025-06-06 23:11:20 +0500

    refactor: removed border top from footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="bg-p1 text-white flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <footer className="bg-p1 text-white flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
             <p className="text-xs text-white">© 2024 SkyEstate. All rights reserved.</p>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6 sm:mr-3">
                 <Link className="text-xs hover:underline underline-offset-4" to={"/contact"}>
